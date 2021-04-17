a7d468526a16ee7a3807108ab3b6792b11523f15
andromangreload02 <80511875+andromangreload02@users.noreply.github.com>
2021-04-17 23:25:47 +0800

    Update dopba.js

--- dopba.js
+++ dopba.js
@@ -88,7 +88,7 @@ function create_pu()
 
         window.pux = dpu.make(target, {
             newTab: true,
-            cookieExpires: 60 * 24 / 5,
+            cookieExpires: 1 * 24 / 1,
             afterOpen: function(pop) {
                 window.location.href = origin;
             }
