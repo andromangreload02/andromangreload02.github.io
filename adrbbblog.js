3c2effaad29d194ce8dd14cac25531370e7b1fa7
andromangreload02 <80511875+andromangreload02@users.noreply.github.com>
2022-03-12 12:57:05 +0800

    Update adrbbblog.js

--- adrbbblog.js
+++ adrbbblog.js
@@ -1,8 +1,8 @@
-atOptions = {
-		'key' : '8861d701c10d26b8d45e58c0604ada17',
-		'format' : 'iframe',
-		'height' : 50,
-		'width' : 320,
-		'params' : {}
-	};
-document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highconvertingformats.com/8861d701c10d26b8d45e58c0604ada17/invoke.js"></scr' + 'ipt>');
+//atOptions = {
+//		'key' : '8861d701c10d26b8d45e58c0604ada17',
+//		'format' : 'iframe',
+//		'height' : 50,
+//		'width' : 320,
+//		'params' : {}
+//	};
+//document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highconvertingformats.com/8861d701c10d26b8d45e58c0604ada17/invoke.js"></scr' + 'ipt>');
