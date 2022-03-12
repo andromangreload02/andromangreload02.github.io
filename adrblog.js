79285ceff46193e361c3da909f7910a5739a5c3f
andromangreload02 <80511875+andromangreload02@users.noreply.github.com>
2022-03-12 10:58:22 +0800

    Create adrblog.js

---
+++ adrblog.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'aa7c11ea78e9d8bcd0d2b7dfb2789b5e',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highconvertingformats.com/aa7c11ea78e9d8bcd0d2b7dfb2789b5e/invoke.js"></scr' + 'ipt>');
