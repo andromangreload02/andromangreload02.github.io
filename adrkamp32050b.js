fcd74659a9155e4d11a1cc53f00f444090042d4f
andromangreload02 <80511875+andromangreload02@users.noreply.github.com>
2024-01-03 07:25:00 +0800

    Create adrkamp32050b.js

---
+++ adrkamp32050b.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '6381fa6911bf0b93d1c83f215cf192e9',
+		'format' : 'iframe',
+		'height' : 50,
+		'width' : 320,
+		'params' : {}
+	};
+document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/6381fa6911bf0b93d1c83f215cf192e9/invoke.js"></scr' + 'ipt>');
