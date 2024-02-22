be5d624e91caa98f21c2f869dcbbf2b02280d695
andromangreload02 <80511875+andromangreload02@users.noreply.github.com>
2024-02-22 19:13:36 +0800

    Update adrbiaa.js

--- adrbiaa.js
+++ adrbiaa.js
@@ -1,8 +1,1 @@
-atOptions = {
-		'key' : 'c0a8a99f155abd38d17acd340d836c98',
-		'format' : 'iframe',
-		'height' : 50,
-		'width' : 320,
-		'params' : {}
-	};
-	document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/c0a8a99f155abd38d17acd340d836c98/invoke.js"></scr' + 'ipt>');
+
