330c531018277c5fe63147aaa58e799065eae51c
andromangreload02 <80511875+andromangreload02@users.noreply.github.com>
2024-02-22 19:31:20 +0800

    Create adrhhhkamp.js

---
+++ adrhhhkamp.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'a31b40d830ed312c9d7fd2c76abd1587',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/a31b40d830ed312c9d7fd2c76abd1587/invoke.js"></scr' + 'ipt>');
