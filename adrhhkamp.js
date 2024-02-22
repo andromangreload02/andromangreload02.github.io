35fba8dba6dfe62107758b78923d162a279b046f
andromangreload02 <80511875+andromangreload02@users.noreply.github.com>
2024-02-22 19:30:07 +0800

    Create adrhhkamp.js

---
+++ adrhhkamp.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'ff1eb0f79fc6bda11aa0e1f3305e9b6f',
+		'format' : 'iframe',
+		'height' : 50,
+		'width' : 320,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/ff1eb0f79fc6bda11aa0e1f3305e9b6f/invoke.js"></scr' + 'ipt>');
