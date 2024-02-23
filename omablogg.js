bbd678f868577d2d4a6d5f81b406d4cb90b75c1d
andromangreload02 <80511875+andromangreload02@users.noreply.github.com>
2024-02-23 17:32:49 +0800

    Update omablogg.js

--- omablogg.js
+++ omablogg.js
@@ -19,7 +19,7 @@
 	}
 
     var direct_link_ads = rChoice([
-                            "https://www.profitablegatecpm.com/abp9rxpru?key=244f42e0fc3af9f066e34636927e676f",
+                            "https://www.profitablegatecpm.com/jp7wt9skzt?key=914f151324404eff1a147945dd4faaff",
                         ]);
 
     var ars             = rChoice([
