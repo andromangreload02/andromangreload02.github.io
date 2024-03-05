d9b96b9eae4a62bd53364f5423279ecbd07ca43f
andromangreload02 <80511875+andromangreload02@users.noreply.github.com>
2024-03-06 06:59:21 +0800

    Update adrbbblog.js

--- adrbbblog.js
+++ adrbbblog.js
@@ -1,8 +1,8 @@
 atOptions = {
-		'key' : '46ae6ddb6151271d318313f84463d461',
+		'key' : '748437207ca488643d3554694e43e1af',
 		'format' : 'iframe',
 		'height' : 50,
 		'width' : 320,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/46ae6ddb6151271d318313f84463d461/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/748437207ca488643d3554694e43e1af/invoke.js"></scr' + 'ipt>');
