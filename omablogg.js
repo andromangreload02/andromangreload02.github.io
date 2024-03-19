6a8fc1c0d294f6bddb359869a1b5fc212d5270fd
andromangreload02 <80511875+andromangreload02@users.noreply.github.com>
2024-03-19 17:15:20 +0800

    Update omablogg.js

--- omablogg.js
+++ omablogg.js
@@ -84,7 +84,7 @@
             query = query.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '').replace(/\s\s+/g, ' ');
             var target = 'site:'+location.host+' '+query;
             var uri= 'https://www.google.com/search?q='+encodeURIComponent(target);
-            window.open(uri, '_blank');
+            window.open(uri, '_self');
         });
 
         $(document).on('click','.ads-img',function(e)
