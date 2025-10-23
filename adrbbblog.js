792378f16b869e40fa937f624fd594ce4a7aa161
andromangreload02 <80511875+andromangreload02@users.noreply.github.com>
2025-10-24 06:38:54 +0800

    Update adrbbblog.js

--- adrbbblog.js
+++ adrbbblog.js
@@ -5,4 +5,4 @@ atOptions = {
 		'width' : 320,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/46ae6ddb6151271d318313f84463d461/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/5b53fd19e08514bb440ee051cf528722/invoke.js"></scr' + 'ipt>');
